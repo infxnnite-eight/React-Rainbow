f6358883dc67d7e345c458b86cecd2894f243c28
infxnnite8 <dorianquintana@gmail.com>
2023-11-07 16:59:59 -0800

    missing-colorform

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import React, { useState } from 'react'
+// Make sure to import the component we just built:
+import ColorBlock from './ColorBlock'
+
+
+function App(){
+    let colors = [
+        'violet', 'blue',
+        'lightblue', 'green',
+        'greenyellow', 'yellow',
+        'orange', 'red'
+    ]
+
+    
+    let colorMap = colors.map((color, i) => {
+        return (
+            <ColorBlock color={color} />
+        )
+    })
+
+    return (
+<div className="App">
+    {colors.map((color, i) => 
+        <ColorBlock key={i} color={color} />
+    )}
+</div>
+
+  
+    )
+}
+
+export default App;
